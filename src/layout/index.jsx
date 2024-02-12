8e7384b2ab99af7484e28ddf027fa04cdda982d5
Majorbangz88 <joelchimaobichukwu@gmail.com>
2024-02-12 21:45:09 +0100

    Updated

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -4,6 +4,7 @@ import BodyContent from "../assets/components/outlet"
 import ThirdSection from "../assets/components/thirdSection"
 import FourthSection from '../assets/components/fourthSection'
 import PaymentSection from "../assets/components/paymentSection"
+import FinalSection from "../assets/components/finalSection"
 
 const Layout = () => {
     return (
@@ -13,6 +14,7 @@ const Layout = () => {
             <ThirdSection />
             <FourthSection />
             <PaymentSection />
+            <FinalSection />
             <Footer/>
         </div>
     )
